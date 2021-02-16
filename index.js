67b81816e71517e005c26e8ac351e4514cfce122
weavermeganmarie <weavermeganmarie@gmail.com>
2021-02-16 13:57:10 -0600

    combining files

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Your index.js is loading correctly!!")
+
+/*scroll to on homepage*/
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
